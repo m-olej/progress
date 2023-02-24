ca678358bbaa82fdf83431c13b15e74e13a3dbf5
Olej <music.olejniczak@wp.pl>
2023-02-24 11:51:33 +0100

    removing the unnecessary directory

---
+++ learning_React/react_project_1/src/App.js
@@ -0,0 +1,12 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
