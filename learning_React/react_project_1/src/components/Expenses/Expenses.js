d4a922d31d4f28ddbde79a558ff7bff47e733b25
Olej <music.olejniczak@wp.pl>
2023-03-01 12:39:06 +0100

    working filtering and list rendering

--- learning_React/react_project_1/src/components/Expenses/Expenses.js
+++ learning_React/react_project_1/src/components/Expenses/Expenses.js
@@ -5,36 +5,19 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
-function Expenses() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+function Expenses(props) {
   const [filterDate, setFilterDate] = useState("2023");
 
   const filterChangeHandler = (enteredFilterDate) => {
     setFilterDate(enteredFilterDate);
-
-    console.log(`In Expenses, ${enteredFilterDate}`);
   };
+
+  console.log({ filterDate });
+
+  const filteredExpenses = props.items.filter((expenses) => {
+    return expenses.date.getFullYear().toString() === filterDate;
+  });
+  console.log(filteredExpenses);
   return (
     <div>
       <Card className="expenses">
@@ -42,34 +25,19 @@ function Expenses() {
           selected={filterDate}
           onChangeFilterDate={filterChangeHandler}
         />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        >
-          {" "}
-        </ExpenseItem>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        >
-          {" "}
-        </ExpenseItem>
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        >
-          {" "}
-        </ExpenseItem>
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        >
-          {" "}
-        </ExpenseItem>
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            // This is needed for keeping order in added elements.
+            // If the added didn't have a unique key atrribute and was
+            // Using state to manage data, there would be a problem with
+            // it not updating properly as the browser couldn't differentiate
+            // between elements
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
